fcf6ba0ec25a7f9400e112c5daad6260188d36d0
MarrVL <mariel.valdez@tecsup.edu.pe>
2026-05-31 15:55:50 -0500

    agregado los puertos de alerta y productos

---
+++ PharmaFlow.Domain/Ports/IRepositorios/IAlertRepository.cs
@@ -0,0 +1,12 @@
+﻿using PharmaFlow.Domain.Entities;
+
+namespace PharmaFlow.Domain.Ports.IRepositorios;
+
+public interface IAlertRepository
+{
+    Task<alerta?> GetByIdAsync(Guid id);
+    Task<IEnumerable<alerta>> GetUnreadAlertsAsync();
+    Task AddAsync(alerta alert);
+    void Update(alerta alert);
+    Task SaveChangesAsync();
+}
---
+++ PharmaFlow.Domain/Ports/IRepositorios/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using PharmaFlow.Domain.Entities;
+
+namespace PharmaFlow.Domain.Ports.IRepositorios;
+
+public interface IProductRepository
+{
+    Task<producto?> GetByIdAsync(Guid id);
+    Task<IEnumerable<producto>> GetAllAsync();
+    Task AddAsync(producto product);
+    void Update(producto product);
+    void Delete(producto product);
+    Task SaveChangesAsync();
+}
