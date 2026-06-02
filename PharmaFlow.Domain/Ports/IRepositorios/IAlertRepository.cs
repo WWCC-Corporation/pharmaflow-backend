cee6b158870a57264697fa1172f218cf343e188f
MarrVL <mariel.valdez@tecsup.edu.pe>
2026-06-01 21:36:41 -0500

    temas solucionados

--- PharmaFlow.Domain/Ports/IRepositorios/IAlertRepository.cs
+++ PharmaFlow.Domain/Ports/IRepositorios/IAlertRepository.cs
@@ -1,12 +1,12 @@
-﻿using PharmaFlow.Domain.Entities;
+﻿using PharmaFlow.Persistence;
 
 namespace PharmaFlow.Domain.Ports.IRepositorios;
 
 public interface IAlertRepository
 {
-    Task<alerta?> GetByIdAsync(Guid id);
-    Task<IEnumerable<alerta>> GetUnreadAlertsAsync();
-    Task AddAsync(alerta alert);
-    void Update(alerta alert);
+    Task<Alerta?> GetByIdAsync(Guid id);
+    Task<IEnumerable<Alerta>> GetUnreadAlertsAsync();
+    Task AddAsync(Alerta alert);
+    void Update(Alerta alert);
     Task SaveChangesAsync();
 }
